c307240ee11aac7f79348b4eff8f74607f6db5bd
AlexandreMath <alemathi@gmail.com>
2016-10-21 09:55:20 +0200

    stage

---
+++ sidebare.php
@@ -0,0 +1,20 @@
+<div>
+	<p>
+		 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque porttitor nisi in nulla lobortis dapibus ut in felis. 
+		 Vivamus imperdiet tortor sem, sit amet efficitur erat luctus in. Quisque iaculis ultrices odio non placerat. 
+		 Donec semper rutrum erat. Sed eget magna urna. In pretium, magna ut cursus sodales, ipsum nibh ullamcorper leo, sed porta tellus nulla vel nisl. 
+		 Curabitur tincidunt ex ut elementum ullamcorper. Ut eu velit vitae quam finibus elementum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
+		 Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum pretium mauris orci, eget hendrerit ipsum fringilla ut. 
+		 In mollis nunc nec ipsum tincidunt dignissim. Morbi varius rhoncus sem. Vestibulum imperdiet eget urna et aliquet.
+
+		Praesent faucibus eleifend efficitur. Fusce et neque non arcu pellentesque lacinia nec ac orci. Vivamus auctor nec ipsum eget dapibus. 
+		Etiam gravida metus at libero rutrum, ultricies pharetra nisl efficitur. Quisque non dolor dignissim, pharetra eros sed, commodo velit. 
+		Vestibulum aliquet erat nec efficitur semper. Suspendisse a pellentesque sem. Sed et aliquam enim.
+
+		Maecenas pharetra egestas metus, ac ultrices velit sagittis eu. Ut urna dui, placerat tempor leo id, consequat mollis velit. Mauris accumsan vestibulum fringilla. 
+		Donec eget placerat lacus, vel varius odio. Nam a leo vitae metus fermentum facilisis in et enim. Nulla facilisi. 
+		Donec vehicula, nisi nec imperdiet tristique, orci risus ultrices velit, non cursus orci mi scelerisque eros. 
+		Etiam mattis ipsum sem, ut egestas sapien bibendum vitae. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+		Sed sagittis ante vitae orci fermentum condimentum. Suspendisse ullamcorper, lectus egestas mollis mattis, nisl mauris consectetur leo, congue sodales justo felis at ante. 
+	</p>
+</div>
